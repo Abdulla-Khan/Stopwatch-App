e766c1c5801986d21d5d22648b1f1a3c3ed6fbfb
Muhammad Abdullah Khan <abdullahkhan.smiu@gmail.com>
2022-02-17 02:14:55 +0500

    Changed Made

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
-// import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,6 +11,18 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  FlutterLocalNotificationsPlugin? local;
+
+  @override
+  void initState() {
+    var and = const AndroidInitializationSettings('icon');
+    var i = const IOSInitializationSettings();
+    var initial = InitializationSettings(android: and, iOS: i);
+    local = FlutterLocalNotificationsPlugin();
+    local!.initialize(initial);
+    super.initState();
+  }
+
   int sec = 0, min = 0, hour = 0;
   String digitSec = '00', digitMin = '00', digitHour = '00';
   Timer? timer;
@@ -47,7 +59,7 @@ class _HomeState extends State<Home> {
 
   void start() {
     started = true;
-    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       int localS = sec + 1;
       int localM = min;
       int localH = hour;
@@ -71,31 +83,40 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future show(String id, desc) async {
+    var detail = const AndroidNotificationDetails('channelId', 'Local',
+        channelDescription: 'Description of Notification',
+        importance: Importance.high);
+    var ios = const IOSNotificationDetails();
+    var gene = NotificationDetails(android: detail, iOS: ios);
+    await local!.show(0, id, desc, gene);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: const Color(0xFF1C2757),
         body: SafeArea(
           child: Padding(
-            padding: EdgeInsets.all(16),
+            padding: const EdgeInsets.all(16),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Center(
+                const Center(
                   child: Text(
-                    'Stpwatch App',
+                    'Stopwatch App',
                     style: TextStyle(
                         color: Colors.white,
                         fontSize: 28.0,
                         fontWeight: FontWeight.bold),
                   ),
                 ),
-                SizedBox(height: 20),
+                const SizedBox(height: 20),
                 Center(
                   child: Text(
                     '$digitHour:$digitMin:$digitSec',
-                    style: TextStyle(
+                    style: const TextStyle(
                         color: Colors.white,
                         fontSize: 82,
                         fontWeight: FontWeight.w600),
@@ -104,7 +125,7 @@ class _HomeState extends State<Home> {
                 Container(
                   height: 400,
                   decoration: BoxDecoration(
-                    color: Color(0xFF323F68),
+                    color: const Color(0xFF323F68),
                     borderRadius: BorderRadius.circular(8),
                   ),
                   child: ListView.builder(
@@ -117,14 +138,14 @@ class _HomeState extends State<Home> {
                           children: [
                             Text(
                               'Lap#${index + 1}',
-                              style: TextStyle(
+                              style: const TextStyle(
                                 color: Colors.white,
                                 fontSize: 16,
                               ),
                             ),
                             Text(
                               '${laps[index]}',
-                              style: TextStyle(
+                              style: const TextStyle(
                                 color: Colors.white,
                                 fontSize: 16,
                               ),
@@ -135,14 +156,14 @@ class _HomeState extends State<Home> {
                                     laps.removeAt(index);
                                   });
                                 },
-                                icon: Icon(Icons.delete))
+                                icon: const Icon(Icons.delete))
                           ],
                         ),
                       );
                     },
                   ),
                 ),
-                SizedBox(height: 20),
+                const SizedBox(height: 20),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -150,33 +171,41 @@ class _HomeState extends State<Home> {
                       child: RawMaterialButton(
                         onPressed: () {
                           (!started) ? start() : stop();
+                          setState(() {
+                            (!started)
+                                ? show(
+                                    'Stopped', '$digitHour:$digitMin:$digitSec')
+                                : show('Started', 'Stopwatch started');
+                          });
                         },
-                        shape:
-                            StadiumBorder(side: BorderSide(color: Colors.blue)),
+                        shape: const StadiumBorder(
+                            side: BorderSide(color: Colors.blue)),
                         child: Text(
                           (!started) ? 'Start' : 'Stop',
-                          style: TextStyle(color: Colors.white),
+                          style: const TextStyle(color: Colors.white),
                         ),
                       ),
                     ),
-                    SizedBox(width: 8),
+                    const SizedBox(width: 8),
                     IconButton(
                         onPressed: () {
                           addLaps();
                         },
-                        icon: Icon(
+                        icon: const Icon(
                           Icons.flag,
                           color: Colors.white,
                         )),
-                    SizedBox(width: 8),
+                    const SizedBox(width: 8),
                     Expanded(
                       child: RawMaterialButton(
                         onPressed: () {
                           reset();
+                          show('Reseted',
+                              'Stopwatch was resetted to  $digitHour:$digitMin:$digitSec');
                         },
-                        shape:
-                            StadiumBorder(side: BorderSide(color: Colors.blue)),
-                        child: Text(
+                        shape: const StadiumBorder(
+                            side: BorderSide(color: Colors.blue)),
+                        child: const Text(
                           'Reset',
                           style: TextStyle(color: Colors.white),
                         ),
